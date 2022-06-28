1c55a959fc98c6ec5a840164dafd28d6eb7396e7
zhoutg <ztg123>
2022-06-28 18:42:41 +0800

    测试服务器是否正常

--- src/main/java/com/ztg/web/RecordController.java
+++ src/main/java/com/ztg/web/RecordController.java
@@ -33,7 +33,6 @@ public class RecordController {
     @Autowired
     RecordFacade recordFacade;
 
-
     @ApiOperation(value = "获取API[zhoutg]",
             notes = "")
     @PostMapping("/getById")
